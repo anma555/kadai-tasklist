d962abaae0558c68c78ea3a9472762218a9737d1
shigeru.yamabayashi <anma555@me.com>
2021-04-18 09:05:39 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   root to: 'tasks#index'
+  
+  get 'signup', to: 'users#new' # /signupで新規登録
+  resources :users, only: [:create]
   
   resources :tasks
 end
